44bbf4f39f3cc4dfa08ce585ec83f18a9abe8e9d
Sarthak Gupta <sarthakgupta007brh@gmail.com>
2025-12-13 19:33:52 +0530

    change

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -1,38 +1,36 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { BsDownload } from "react-icons/bs";
+import pdf from "../Resume.pdf";
 import { Document, Page, pdfjs } from 'react-pdf';
 import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
 import 'react-pdf/dist/esm/Page/TextLayer.css';
+pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`;
 
-pdfjs.GlobalWorkerOptions.workerSrc = 
-  `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`;
 
 const Resume = () => {
-  const [wid, setWid] = useState(window.innerWidth);
-  const pdfUrl = "/SarthakGupta_Resume.pdf";
+  const[wid, setwid]=useState(window.innerWidth);
 
-  useEffect(() => {
-    const handleResize = () => setWid(window.innerWidth);
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
+  const handleResize=()=>{
+    setwid(window.innerWidth);
+  }
+
+  window.addEventListener("load", handleResize);
+  window.addEventListener("resize", handleResize);
 
   return (
-    <div className="ResumePage">
-      <Document file={pdfUrl} className="resumeview">
-        <Page
-          pageNumber={1}
-          scale={wid < 700 ? (wid > 475 ? 0.7 : 0.5) : 1}
-        />
+    <div className='ResumePage'>
+      <Document file={pdf} className="resumeview">
+          <Page pageNumber= {1} scale={wid<700 ? ( wid>475? 0.7: 0.5): 1}/>
       </Document>
 
-      <a href={pdfUrl} download="SarthakGupta_Resume.pdf">
-        <button className="downloadCV" type="button">
-          <h3><BsDownload />&nbsp; Download CV</h3>
+      <a href={pdf} target='_blank' download="Sarthak's Resume">
+        <button className='downloadCV' type='button'>
+          <h3><BsDownload/>&nbsp; Download CV</h3>
         </button>
       </a>
-    </div>
-  );
-};
 
-export default Resume;
+    </div>
+  )
+}
+
+export default Resume
